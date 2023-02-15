3ce1a046d3e3eb6750c35058cf62ed702fc5723a
Riccardo Caprile <93477707+Riz97@users.noreply.github.com>
2023-02-16 00:00:30 +0100

    .

--- js/developed/developed_plot1.js
+++ js/developed/developed_plot1.js
@@ -4,34 +4,48 @@
 
 const id_ref_1 = "#choropleth-map-developed"
 
-const svg = d3.select("svg"),
-  width = +svg.attr("width"),
-  height = +svg.attr("height");
+
+// The svg
+// Set the dimensions and margins of the graph
+const margin_1 = { top: 50, right: 20, bottom: 70, left: 20 },
+    width_1 = 1024 - margin_1.left - margin_1.right,
+    height_1 = 768 - margin_1.top - margin_1.bottom;
+    
+// Scale factor on both dimensions (width and height)
+const scaleFactor_1 = 0.9;
+
+// Append the svg_1 object to the page
+const svg_1 = d3.select(id_ref_1)
+    .append("svg")
+    //.attr("width", width_1 + margin_1.left + margin_1.right)
+    //.attr("height", height_1 + margin_1.top + margin_1.bottom)
+    .attr("viewBox", '0 0 ' + (width_1 + margin_1.left + margin_1.right) +
+        ' ' + (height_1 + margin_1.top + margin_1.bottom))
+    .append("g")
+    .attr("transform", `translate(${(1-scaleFactor_1)*width_1/2 + margin_1.left},
+                                  ${(1-scaleFactor_1)*height_1/2 + margin_1.top})`);
 
 // Map and projection
 const path = d3.geoPath();
-const projection = d3.geoMercator()
-  .scale(70)
-  .center([0,20])
-  .translate([width / 2, height / 2]);
+var projection = d3.geoMercator().center([0,0]).scale(150);
 
 // Data and color scale
 let data = new Map()
 const colorScale = d3.scaleThreshold()
-  .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
+  .domain([73, 75, 77, 79, 80, 81])
   .range(d3.schemeBlues[7]);
 
 // Load external data and boot
 Promise.all([
-d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"),
-d3.csv("", function(d) {
-    data.set(d.code, +d.pop)
+d3.json("../../data/world.geojson"),
+d3.csv("../../data/developed/developed1-2.csv", function(d) {
+    data.set(d.Country, +d.Life_expectancy)
 })
 ]).then(function(loadData){
     let topo = loadData[0]
 
     // Draw the map
-  svg.append("g")
+  svg_1.append("g")
     .selectAll("path")
     .data(topo.features)
     .join("path")
