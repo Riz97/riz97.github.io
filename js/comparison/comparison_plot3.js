ae412128ff0d035b89c4b0cf37e511aee9f61f65
Davide Marinelli <davidemari98@gmail.com>
2023-02-20 14:10:01 +0100

    .

--- js/comparison/comparison_plot3.js
+++ js/comparison/comparison_plot3.js
@@ -1,122 +1,74 @@
 const id_ref_3 = "#ridgeline"
+// set the dimensions and margin_3s of the graph
+var margin_3 = {top: 10, right: 30, bottom: 30, left: 40},
+    width_3 = 460 - margin_3.left - margin_3.right,
+    height_3 = 400 - margin_3.top - margin_3.bottom;
 
-// Set the dimensions and margins of the graph
-const margin_3 = { top: 200, right: 20, bottom: 70, left: 75 },
-    width_3 = 1024 - margin_3.left - margin_3.right,
-    height_3 = 768 - margin_3.top - margin_3.bottom;
+// append the svg object to the body of the page
+var svg_3 = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width_3", width_3 + margin_3.left + margin_3.right)
+    .attr("height_3", height_3 + margin_3.top + margin_3.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin_3.left + "," + margin_3.top + ")");
 
+// get the data
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_doubleHist.csv", function(data) {
 
-// Append the svg_3 object to the page
-const svg_3 = d3.select(id_ref_3)
-    .append("svg")
-    //.attr("width", width_3 + margin_3.left + margin_3.right)
-    //.attr("height", height_3 + margin_3.top + margin_3.bottom)
-    .attr("viewBox", '0 0 ' + (width_3 + margin_3.left + margin_3.right) +
-        ' ' + (height_3 + margin_3.top + margin_3.bottom))
-    .append("g")
-    .attr("transform", `translate(${margin_3.left}, ${margin_3.top})`);
+  // X axis: scale and draw:
+  var x = d3.scaleLinear()
+      .domain([-4,9])     // can use this instead of 1000 to have the max of data: d3.max(data, function(d) { return +d.price })
+      .range([0, width_3]);
+  svg_3.append("g")
+      .attr("transform", "translate(0," + height_3 + ")")
+      .call(d3.axisBottom(x));
 
-svg_3.append("text")
-.text("Min and Max temperature distribution by month of the 1993")
-.attr("y", -150)
-.attr("id", "ridgeTitle")
-.style("class", "h2")
-.style("font-size", "18px")
-.attr("text-anchor", "left")  
-.style("text-decoration", "underline")
+  // set the parameters for the histogram
+  var histogram = d3.histogram()
+      .value(function(d) { return +d.value; })   // I need to give the vector of value
+      .domain(x.domain())  // then the domain of the graphic
+      .thresholds(x.ticks(40)); // then the numbers of bins
 
-svg_3.append("text")
-.text("Temperature, °C")
-.attr("text-anchor", "end")
-.attr("x", width_3)
-.attr("y", height_3 + margin_3.bottom- 5)
+  // And apply twice this function to data to get the bins.
+  var bins1 = histogram(data.filter( function(d){return d.type === "variable 1"} ));
+  var bins2 = histogram(data.filter( function(d){return d.type === "variable 2"} ));
 
-var groupBy = function(xs, key) {
-    return xs.reduce(function(rv, x) {
-        (rv[x[key]] = rv[x[key]] || []).push(x);
-        return rv;
-    }, {});
-    };
+  // Y axis: scale and draw:
+  var y = d3.scaleLinear()
+      .range([height_3, 0]);
+      y.domain([0, d3.max(bins1, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+  svg_3.append("g")
+      .call(d3.axisLeft(y));
 
-function kernelDensityEstimator(kernel, X) {
-    return function(V) {
-        return X.map(function(x) {
-        return [x, d3.mean(V, function(v) { return kernel(x - v); })];
-        });
-    };
-}
-function kernelEpanechnikov(k) {
-    return function(v) {
-        return Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0;
-    };
-}
+  // append the bars for series 1
+  svg_3.selectAll("rect")
+      .data(bins1)
+      .enter()
+      .append("rect")
+        .attr("x", 1)
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("width_3", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+        .attr("height_3", function(d) { return height_3 - y(d.length); })
+        .style("fill", "#69b3a2")
+        .style("opacity", 0.6)
 
-var years_3 = new Set
-var names = ["2014","2015"]
+  // append the bars for series 2
+  svg_3.selectAll("rect2")
+      .data(bins2)
+      .enter()
+      .append("rect")
+        .attr("x", 1)
+        .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+        .attr("width_3", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+        .attr("height_3", function(d) { return height_3 - y(d.length); })
+        .style("fill", "#404080")
+        .style("opacity", 0.6)
 
-var min, max, xAxis, yBandAxis, yDensityAxis, byYear
-//const selectionBox = document.getElementById("ridgeSelection")
+  // Handmade legend
+  svg_3.append("circle").attr("cx",300).attr("cy",30).attr("r", 6).style("fill", "#69b3a2")
+  svg_3.append("circle").attr("cx",300).attr("cy",60).attr("r", 6).style("fill", "#404080")
+  svg_3.append("text").attr("x", 320).attr("y", 30).text("variable A").style("font-size", "15px").attr("alignment-baseline","middle")
+  svg_3.append("text").attr("x", 320).attr("y", 60).text("variable B").style("font-size", "15px").attr("alignment-baseline","middle")
 
-
-d3.csv("../../data/comparison/comparison-3.csv").then( function (data)
-{
-    // for(i = 0; i < data.length; ++i)
-    //     years_3.add(data[i]['year']) 
-    
-    // Array.from(years_3).forEach(function (el){
-    //     selectionBox.appendChild(new Option(el, el))
-    // })
-    
-
-    min = data.reduce((acc, el) => Math.min(acc, el.L_E_Developing), Infinity)
-    max = data.reduce((acc, el) => Math.max(acc, el.L_E_Developed), -Infinity)
-    
-    xAxis = d3.scaleLinear().domain([min - 0.01, max + 0.01]).range([0, width_3])
-    yBandAxis = d3.scalePoint().range([0, height_3]).domain(names)
-    yDensityAxis = d3.scaleLinear().domain([0, 0.4]).range([height_3, 0])
-    
-    svg_3.append("g").attr("transform", `translate(0, ${height_3})`).call(d3.axisBottom(xAxis).tickFormat(d => d +"°"))
-    svg_3.append("g").call(d3.axisLeft(yBandAxis))
-    
-    
-    //byYear = groupBy(data, "year")
-    
-    draw()
-})
-
-function draw()
-{
-    // var year = selectionBox.value
-    const kde = kernelDensityEstimator(kernelEpanechnikov(7), xAxis.ticks(80))
-    
-    chartTitle = document.getElementById("ridgeTitle").textContent = `Min and Max temperature distribution by month of the `
-
-    //var byYear = groupBy(Year, "Year")
-    var densities = Array()
-    
-    for(var i = 0; i < 2; ++i)
-    {
-        // Non sono convintissimo che mi piaccia, ma almeno esce giusto
-        var mins = [[min -0.01, 0]].concat(kde(el => el.L_E_Developing))
-        var maxs = [[min -0.01, 0]].concat(kde(el => el.L_E_Developed))
-        mins.push([max + 0.01, 0])
-        maxs.push([max + 0.01, 0])
-        
-        // Le metto così in modo che le disegni in ordine, che è l'unico modo che ho trovato per mmetterli uno sopra l'altro
-        densities.push({"month": names[i], "density" : mins, "type": "min"})
-        densities.push({"month": names[i], "density" : maxs, "type": "max"})
-    }
-    
-    svg_3.selectAll(".areas")
-    .data(densities)
-    .join("path")
-    .attr("class", "areas")
-    .attr("transform", d => `translate(0, ${(yBandAxis(d.month) - height_3)})`)
-    .attr("fill", d => d.type === "min" ? "blue" : "red")
-    // .attr("opacity",  0.8) // Non so, non mi piace
-    .datum(d => d.density)
-    .attr("stroke", "black")
-    .attr("stroke-width", "2")
-    .attr("d", d3.line().curve(d3.curveBasis).x(d => xAxis(d[0])).y(d => yDensityAxis(d[1])))
-
-}
+});
